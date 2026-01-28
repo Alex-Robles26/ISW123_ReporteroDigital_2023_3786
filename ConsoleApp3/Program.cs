f52e944b4901f3400110d2cc5455d4aba6fea5cc
Alex Robles <alexroblescarpioz@gmail.com>
2026-01-27 20:12:56 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ISW123_ReporteroDigital_2023_3786
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Periódico Digital Dominicano");
+            Console.WriteLine("Desarrollador: Manaury Roble");
+            Console.WriteLine("Matrícula: 2023-3786");
+            Console.ReadKey();
+        }
+    }
+}
